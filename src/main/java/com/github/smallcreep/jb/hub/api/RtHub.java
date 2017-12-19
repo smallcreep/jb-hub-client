55860e970343e6aaae8c2a185f4252350edc8dab
smallcreep <ilia.rogozhin@gmail.com>
2017-12-19 23:59:31 +0300

    #45 Pdd error

--- src/main/java/com/github/smallcreep/jb/hub/api/RtHub.java
+++ src/main/java/com/github/smallcreep/jb/hub/api/RtHub.java
@@ -121,9 +121,12 @@ public final class RtHub implements Hub {
 
     @Override
     public Permissions permissions() {
-        // @todo #45:30m/DEV Implement Permissions class.
-        //Add class RtPermissions with all methods
-        //and JsPermission with all methods.
+        // @checkstyle RegexpMultilineCheck (2 lines)
+        /**
+         * @todo #45:30m/DEV Implement Permissions class.
+         *  Add class RtPermissions with all methods
+         *  and JsPermission with all methods.
+         */
         return null;
     }
 }
